fc020750684293e6a3e6be11c3c4724716c035d6
erikest <erikest@gmail.com>
2019-04-05 10:42:57 -0700

    Update with --existing and -gsc options.  Refactored to use a class to hold project info.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
+using System.Security;
 
 
 //Thank you: https://blog.maartenballiauw.be/post/2017/04/10/extending-dotnet-cli-with-custom-tools.html, for clarity on CLI extensions
@@ -22,87 +23,84 @@ namespace gitstub
         /// -gsr repository/project name to create
         /// -gss solution/repository name [OPTIONAL]
         /// -gsd description of new project [OPTIONAL]
-        /// -gaa access, pub for public, private otherwise [OPTIONAL]
+        /// -gsa access, pub for public, private otherwise [OPTIONAL]
+        /// -gsc commit message [OPTIONAL]
+        /// --existing, use the existing solution/project in the current working directory, instead of creating a new project/solution.  Note: this will still add .gitignore [OPTIONAL]
         /// </param>
         static void Main(string[] args)
         {
-            //1: Parse Args
-            string user, pass, project, solution, desc, accessPrivate;
-            List<string> dotnetNewArgs;
-            ParseArguments(args, out user, out pass, out project, out solution, out desc, out accessPrivate, out dotnetNewArgs);
+            //1: Parse Args            
+            var project = ParseArguments(args);
 
-            //2:  Make API Call to create the github repository
-            string auth, repoName;
-            CreateGitHubRepository(user, pass, project, solution, desc, accessPrivate, out auth, out repoName);
+            //2:  Make API Call to create the github repository            
+            CreateGitHubRepository(project);
 
             //3:  Run Git Commands - Let's get ready
-            InitializeLocalRepository(user, auth, repoName);
+            InitializeLocalRepository(project);
 
             //4:  Create new project - To Rumble!
-            CreateProjectStructure(project, solution, dotnetNewArgs);
+            CreateProjectStructure(project);
 
             //5:  Add, Commit, Push - ohhh, feel the power!
-            AddCommitPush();
+            AddCommitPush(project);
         }
 
-        private static void AddCommitPush()
+        private static void AddCommitPush(GitStubProject project)
         {
             RunGit("add -A"); //Add all the files in the directory
-            RunGit("commit -m \"gitstub => Project Initialization\"");
+            RunGit($"commit -m \"{project.CommitMessage}\"");
             RunGit("push --set-upstream origin master");
         }
 
-        private static void CreateProjectStructure(string project, string solution, List<string> dotnetNewArgs)
+        private static void CreateProjectStructure(GitStubProject project)
         {
-            if (!string.IsNullOrWhiteSpace(solution)) //if solution was specified, make a simple structure
+            if (!project.UseExisting)
             {
-                //Create the folder structure =>src/Proj/
-                var projSubDir = $"src\\{project}";
-                Directory.CreateDirectory(projSubDir);
-                RunDotnetNew($"sln -n {solution}"); //Create the solution file
+                if (!string.IsNullOrWhiteSpace(project.Solution)) //if solution was specified, make a simple structure
+                {
+                    //Create the folder structure =>src/Proj/
+                    var projSubDir = $"src\\{project.Project}";
+                    Directory.CreateDirectory(projSubDir);
+                    RunDotnetNew($"sln -n {project.Solution}"); //Create the solution file
 
-                //Create the project in src/project
-                RunDotnetNew(string.Join(" ", dotnetNewArgs), $"\\{projSubDir}");
-                RunDotnetSln($"add {projSubDir}"); //Add the new project to the solution
+                    //Create the project in src/project
+                    RunDotnetNew(string.Join(" ", project.NewArgs), $"\\{projSubDir}");
+                    RunDotnetSln($"add {projSubDir}"); //Add the new project to the solution
 
-                //TODO add tests/testProject and any other structure scaffolding
+                    //TODO add tests/testProject and any other structure scaffolding
+                }
+                else
+                    RunDotnetNew(string.Join(" ", project.NewArgs));
             }
-            else
-                RunDotnetNew(string.Join(" ", dotnetNewArgs));
 
             //Add .gitignore
             //Thank you: https://github.com/dotnet/core/blob/master/.gitignore
             File.WriteAllText(".gitignore", gitIgnores(), System.Text.Encoding.ASCII);
         }
 
-        private static void InitializeLocalRepository(string user, string auth, string repoName)
+        private static void InitializeLocalRepository(GitStubProject project)
         {
             //Initialize the local git repo
             var gitArgs = "init";
             RunGit(gitArgs);
 
             //Point the repository to github location
-            RunGit($"remote add origin https://{auth}@github.com/{user}/{repoName}.git");
+            RunGit($"remote add origin https://{project.Auth}@github.com/{project.User}/{project.RepoName}.git");
         }
 
-        private static void CreateGitHubRepository(string user, string pass, string project, string solution, string desc, string accessPrivate, out string auth, out string repoName)
+        private static void CreateGitHubRepository(GitStubProject project)
         {
             ////    This can be extended to provide more parameters to creation, in addition to name and description
-
-            //Thank you: https://stackoverflow.com/questions/2423777/is-it-possible-to-create-a-remote-repo-on-github-from-the-cli-without-opening-br
-            var gitHubApiUrl = "https://api.github.com/user/repos";
-            auth = $"{user}:{pass}";
-            repoName = solution ?? project;
             //Thank you: https://stackoverflow.com/questions/18971510/how-do-i-set-up-httpcontent-for-my-httpclient-postasync-second-parameter
-            var apiValues = $"{{\"name\":\"{repoName}\", \"description\":\"{desc}\", \"private\":\"{accessPrivate}\"}}";
+            var apiValues = $"{{\"name\":\"{project.RepoName}\", \"description\":\"{project.Desc}\", \"private\":\"{project.AccessPrivate}\"}}";
             var apiContent = new StringContent(apiValues, System.Text.Encoding.UTF8, "application/json");
             using (var client = new HttpClient())
             {
                 try
                 {
-                    HttpResponseMessage response = CreateGitHubRepository(user, project, auth, gitHubApiUrl, apiContent, client);
+                    HttpResponseMessage response = CreateGitHubRepository(project, apiContent, client);
 
-                    ProcessGitHubAPIResponse(user, repoName, response);
+                    ProcessGitHubAPIResponse(project, response);
                 }
                 catch (HttpRequestException reqEx)
                 {
@@ -111,24 +109,26 @@ namespace gitstub
             }
         }
 
-        private static HttpResponseMessage CreateGitHubRepository(string user, string project, string auth, string gitHubApiUrl, StringContent apiContent, HttpClient client)
+        private static HttpResponseMessage CreateGitHubRepository(GitStubProject project, StringContent apiContent, HttpClient client)
         {
+            //Thank you: https://stackoverflow.com/questions/2423777/is-it-possible-to-create-a-remote-repo-on-github-from-the-cli-without-opening-br
+            var gitHubApiUrl = "https://api.github.com/user/repos";
             //Thank you: https://stackoverflow.com/questions/50732772/best-curl-u-equivalence-in-c-sharp
-            client.DefaultRequestHeaders.Add("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(auth)));
+            client.DefaultRequestHeaders.Add("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(project.Auth)));
             //Thank you: https://stackoverflow.com/questions/39907742/github-api-is-responding-with-a-403-when-using-requests-request-function
-            client.DefaultRequestHeaders.Add("User-Agent", $"{user}/{project}");
+            client.DefaultRequestHeaders.Add("User-Agent", $"{project.User}/{project.RepoName}");
 
             var response = client.PostAsync(gitHubApiUrl, apiContent).Result; 
             return response;
         }
 
-        private static void ProcessGitHubAPIResponse(string user, string repoName, HttpResponseMessage response)
+        private static void ProcessGitHubAPIResponse(GitStubProject project, HttpResponseMessage response)
         {
             (string code, string msg) err = (null, null);
             switch (response.StatusCode)
             {
                 case HttpStatusCode.Created: //SUCCESS!! If only it was so easy in the rest of your life...
-                    Console.WriteLine($"GitHub Repository Created: https://github.com/{user}/{repoName}"); break;
+                    Console.WriteLine($"GitHub Repository Created: https://github.com/{project.User}/{project.RepoName}"); break;
                 case HttpStatusCode.NotFound:
                     err = ("404", "Not Found.  In this context, it probably means a problem with your username or password"); break;
                 case HttpStatusCode.Forbidden:
@@ -146,30 +146,24 @@ namespace gitstub
             }
         }
 
-        private static void ParseArguments(string[] args, out string user, out string pass, out string project, out string solution, out string desc, out string accessPrivate, out List<string> dotnetNewArgs)
+        private static GitStubProject ParseArguments(string[] args)
         {
             ////Help me Hanselman: Better to have options not to need username and pass, like supporting SSH, or caching credentials in a secrets manager or some such, right?
-            user = null;
-            pass = null;
-            project = null;
-            solution = null;
-            desc = "";
-            accessPrivate = "true";
-            dotnetNewArgs = new List<string>();
+            var project = ProcessArgsArray(args);
 
-            ProcessArgsArray(args, ref user, ref pass, ref project, ref solution, ref desc, ref accessPrivate, dotnetNewArgs);
+            ValidateParameters(project);
 
-            ValidateParameters(user, pass, project);
+            return project;
         }
 
-        private static void ValidateParameters(string user, string pass, string project)
+        private static void ValidateParameters(GitStubProject project)
         {
             List<(string field, string param)> errs = new List<(string, string)>();
-            if (string.IsNullOrWhiteSpace(user))
+            if (string.IsNullOrWhiteSpace(project.User))
                 errs.Add(("your GitHub username", "-gsa <username>"));
-            if (string.IsNullOrWhiteSpace(pass))
+            if (string.IsNullOrWhiteSpace(project.Pass))
                 errs.Add(("your GitHub password", "-gsp <password>"));
-            if (string.IsNullOrWhiteSpace(project))
+            if (string.IsNullOrWhiteSpace(project.Project))
                 errs.Add(("the Repository/Project to create", "-gsr <repository>"));
 
             if (errs.Count > 0)
@@ -179,8 +173,9 @@ namespace gitstub
             }
         }
 
-        private static void ProcessArgsArray(string[] args, ref string user, ref string pass, ref string project, ref string solution, ref string desc, ref string accessPrivate, List<string> dotnetNewArgs)
+        private static GitStubProject ProcessArgsArray(string[] args)
         {
+            GitStubProject project = new GitStubProject();
             try
             {
                 for (int i = 0; i < args.Length; i++)
@@ -188,26 +183,33 @@ namespace gitstub
                     switch (args[i].ToLower())
                     {
                         case "-gsu":
-                            user = args[++i]; break;
+                            project.User = args[++i]; break;
                         case "-gsp":
-                            pass = args[++i]; break;
+                            project.Pass = args[++i]; break;
                         case "-gsr":
-                            project = args[++i]; break;
+                            project.Project = args[++i]; break;
                         case "-gss":
-                            solution = args[++i]; break;
+                            project.Solution = args[++i]; break;
                         case "-gsd":
-                            desc = args[++i]; break;
+                            project.Desc = args[++i]; break;
+                        case "-gsc":
+                            project.CommitMessage = args[++i]; break;
                         case "-gsa":
-                            accessPrivate = args[++i].ToLower() == "pub" ? "false" : "true"; break;
+                            project.AccessPrivate = args[++i].ToLower() == "pub" ? "false" : "true"; break;
+                        case "--existing":
+                            project.UseExisting = true; break;
                         default:
-                            dotnetNewArgs.Add(args[i]); break;
+                            project.NewArgs.Add(args[i]); break;
                     }
                 }
             }
             catch (IndexOutOfRangeException outofRange)
             {
                 Console.WriteLine("Error: Invalid Parameters.  It's not you baby, it's just the params.");
+                Environment.Exit(2);
             }
+
+            return project;
         }
 
         //These should probably monitor the std or err out and do nice, gentle things when git gets rough.
@@ -280,5 +282,22 @@ msbuild.wrn
 # Visual Studio 2015
 .vs / ";
         }
+    }
+
+    internal class GitStubProject
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string Project { get; set; }
+        public string Solution { get; set; }
+        public string Desc { get; set; }
+        public string CommitMessage { get; set; } = "GitStub => Project Initialization!";
+        public string AccessPrivate { get; set; }
+        public bool UseExisting { get; set; }
+        public List<string> NewArgs { get; set; } = new List<string>();
+
+        public string Auth => $"{User}:{Pass}";
+
+        public string RepoName => Solution ?? Project;
     }
 }
